68597cf75074f36877eac5ffe5dce1f24aae6528
AMROUNE HOUSSAM EDDINE <92638543+Amroune28@users.noreply.github.com>
2025-11-30 21:26:40 +0100

    Delete Kotlin directory

--- Kotlin/src/Comments.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-
-    // This is test print ..
-    println("Welcome Kotlin !")
-
-}
--- Kotlin/src/Variables.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-
-    var name : String = "Houari"
-
-    var num : Int
-    num = -20
-
-    println(num)
-}
